91ba83d82901bfd646e593b516d37c6d7f4184ef
Camilo <camilo.velezp@udea.edu.co>
2022-06-21 19:20:22 -0500

    origin again

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.get("/", function (request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
+// Serve the assets directory
+app.use('/assets',express.static('assets'))
+
 // Listen on port 5000
 app.set('port', (process.env.PORT || 5000));
 http.listen(app.get('port'), function(){
